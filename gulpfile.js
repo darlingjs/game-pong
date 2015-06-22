239a8441b7bf07755a88abd492cab7defacd934c
Eugene Krevenets <hyzhak@ya.ru>
2015-06-22 16:09:43 +0200

    add build script and start using browerify

---
+++ gulpfile.js
@@ -0,0 +1,92 @@
+var del = require('del');
+var gulp = require('gulp');
+var browserify = require('gulp-browserify');
+var debug = require('gulp-debug');
+var less = require('gulp-less');
+var processhtml = require('gulp-processhtml');
+var sourcemaps = require('gulp-sourcemaps');
+var uglify = require('gulp-uglify');
+
+var LessPluginAutoPrefix = require('less-plugin-autoprefix'),
+    //uses Montenegro usage statistics. It accepts two-letter country code.
+    autoprefixPlugin = new LessPluginAutoPrefix({browsers: ['> 1% in ME']});
+
+var LessPluginCleanCSS = require('less-plugin-clean-css'),
+    cleancss = new LessPluginCleanCSS({ advanced: true });
+
+var paths = {
+    scripts: {
+        source: ['./js/game.js'],
+        output: {
+            path: './build/js',
+            filename: 'game.min.js'
+        }
+    },
+    styles: {
+        source: './styles/*.less',
+        output: {
+            path: './build/styles',
+            filename: 'game.css'
+        }
+    },
+    html: {
+        source: './index.html',
+        output: {
+            path: './build',
+            filename: 'index'
+        }
+    }
+};
+
+function clean(done) {
+    del(['build'], done);
+}
+
+function html() {
+    return gulp.src(paths.html.source)
+        .pipe(processhtml())
+        .pipe(debug({title: 'html:'}))
+        .pipe(gulp.dest(paths.html.output.path));
+}
+
+function styles() {
+    return gulp.src(paths.styles.source)
+        //TODO: sourcemap doesn't work with plugins because of bug in them
+        //.pipe(sourcemaps.init())
+        .pipe(less({
+            plugins: [cleancss, autoprefixPlugin]
+        }))
+        //.pipe(sourcemaps.write('.'))
+        .pipe(debug({title: 'styles:'}))
+        .pipe(gulp.dest(paths.styles.output.path));
+}
+
+function scripts() {
+    return gulp.src(paths.scripts.source)
+        .pipe(sourcemaps.init())
+        .pipe(browserify({
+            //debug : !gulp.env.production
+        }))
+        .pipe(uglify())
+        .pipe(sourcemaps.write('.'))
+        .pipe(debug({title: 'scripts:'}))
+        .pipe(gulp.dest(paths.scripts.output.path));
+}
+
+gulp.task('clean', clean);
+gulp.task('html', html);
+gulp.task('styles', styles);
+gulp.task('scripts', scripts);
+
+gulp.task('watch', function() {
+    gulp.watch(paths.html.source, html);
+    gulp.watch(paths.styles.source, styles);
+    gulp.watch(paths.scripts.source, scripts);
+});
+
+gulp.task('build', gulp.series(
+    clean,
+    gulp.parallel('scripts', 'html', 'styles')
+));
+
+gulp.task('default', gulp.series('build'));
--- js/dom.js
+++ js/dom.js
@@ -2,136 +2,136 @@
  * Dom Renderer Module
  */
 
-(function() {
-    'use strict';
+'use strict';
 
-    //create DOM module
-    var m = darlingjs.module('domModule', []);
+var darlingjs = require('darlingjs');
 
-    /**
-     * DOM component
-     */
-    m.$c('domView', {
-        //color of DOM element
-        color: null,
+//create DOM module
+var m = darlingjs.module('domModule', []);
 
-        //element for temporary storing
-        element: null
-    });
+/**
+ * DOM component
+ */
+m.$c('domView', {
+    //color of DOM element
+    color: null,
 
-    /**
-     * System that renderer entity into the DOM
-     */
-    m.$system('domViewRenderer', {
+    //element for temporary storing
+    element: null
+});
 
-        //for with entity that holds:
-        $require: ['ng2D', 'ng2DSize', 'domView'],
+/**
+ * System that renderer entity into the DOM
+ */
+m.$system('domViewRenderer', {
 
-        target: null,
+    //for with entity that holds:
+    $require: ['ng2D', 'ng2DSize', 'domView'],
 
-        width: 400,
-        height: 300,
+    target: null,
 
-        backgroundColor: 'rgb(0,0,0)',
+    width: 400,
+    height: 300,
 
-        //@private
-        stageElement: null,
+    backgroundColor: 'rgb(0,0,0)',
 
-        //@private
-        updatePosition: null,
+    //@private
+    stageElement: null,
 
-        /**
-         * System added to the World
-         */
-        $added: function() {
-            this.stageElement = document.querySelector(this.target);
-            if (!this.stageElement) {
-                throw new Error('can\'t find ' + id + ' as stage for dom renderer')
-            }
-
-            //fit stage DIV element to the size
-            this.stageElement.style.backgroundColor = this.backgroundColor;
-            this.stageElement.style.width = this.width + 'px';
-            this.stageElement.style.height = this.height + 'px';
-            this.stageElement.style.margin = '0 auto';
-
-            this.updatePosition = buildUpdatePosition(getVendorPrefix());
-        },
-
-        /**
-         * System removed from the World
-         */
-        $removed: function() {
-            this.stageElement = null;
-        },
-
-        /**
-         * Add new entity to the System
-         */
-        $addEntity: ['$entity', function($entity) {
-            var domView = $entity.domView,
-                ng2DSize = $entity.ng2DSize;
-
-            //create DIV for each entity
-            var element = document.createElement('div');
-
-            element.style.backgroundColor = domView.color;
-            element.style.width = ng2DSize.width + 'px';
-            element.style.height = ng2DSize.height + 'px';
-
-            element.style.position = 'absolute';
-
-            this.stageElement.appendChild(element);
-            this.updatePosition(element, $entity.ng2D, $entity.ng2DSize);
-            domView.element = element;
-        }],
-
-        /**
-         * Remove entity from the System
-         */
-        $removeEntity: ['$entity', function($entity) {
-            var domView = $entity.domView;
-            this.stageElement.removeChild(domView.element);
-            domView.element = null;
-        }],
-
-        /**
-         * Update position of DIV elements each tick
-         */
-        $update: ['$entity', function($entity) {
-            this.updatePosition($entity.domView.element, $entity.ng2D, $entity.ng2DSize);
-        }]
-    });
+    //@private
+    updatePosition: null,
 
     /**
-     * Get Vendor Prefix. Based on CraftyJS <http://craftyjs.com>
-     * @returns {*}
+     * System added to the World
      */
-    function getVendorPrefix() {
-        var ua = navigator.userAgent.toLowerCase(),
-            match = /(webkit)[ \/]([\w.]+)/.exec(ua) ||
-                /(o)pera(?:.*version)?[ \/]([\w.]+)/.exec(ua) ||
-                /(ms)ie ([\w.]+)/.exec(ua) ||
-                /(moz)illa(?:.*? rv:([\w.]+))?/.exec(ua) || [],
-            prefix = (match[1] || match[0]);
-
-        //browser specific quirks
-        if (prefix === "moz") prefix = "Moz";
-        if (prefix === "o") prefix = "O";
-        return prefix;
-    }
-
-    /**
-     * Update position of DOM element in WebKit style
-     * @param element
-     * @param ng2D
-     */
-    function buildUpdatePosition(prefix) {
-        var property = prefix + 'Transform';
-        return function (element, ng2D, ng2DSize) {
-            element.style[property] = 'translate3d('
-                    + ~~(ng2D.x - 0.5 * ng2DSize.width) + 'px, '
-                    + ~~(ng2D.y - 0.5 * ng2DSize.height) + 'px, 0)';
+    $added: function() {
+        this.stageElement = document.querySelector(this.target);
+        if (!this.stageElement) {
+            throw new Error('can\'t find ' + id + ' as stage for dom renderer')
         }
+
+        //fit stage DIV element to the size
+        this.stageElement.style.backgroundColor = this.backgroundColor;
+        this.stageElement.style.width = this.width + 'px';
+        this.stageElement.style.height = this.height + 'px';
+        this.stageElement.style.margin = '0 auto';
+
+        this.updatePosition = buildUpdatePosition(getVendorPrefix());
+    },
+
+    /**
+     * System removed from the World
+     */
+    $removed: function() {
+        this.stageElement = null;
+    },
+
+    /**
+     * Add new entity to the System
+     */
+    $addEntity: ['$entity', function($entity) {
+        var domView = $entity.domView,
+            ng2DSize = $entity.ng2DSize;
+
+        //create DIV for each entity
+        var element = document.createElement('div');
+
+        element.style.backgroundColor = domView.color;
+        element.style.width = ng2DSize.width + 'px';
+        element.style.height = ng2DSize.height + 'px';
+
+        element.style.position = 'absolute';
+
+        this.stageElement.appendChild(element);
+        this.updatePosition(element, $entity.ng2D, $entity.ng2DSize);
+        domView.element = element;
+    }],
+
+    /**
+     * Remove entity from the System
+     */
+    $removeEntity: ['$entity', function($entity) {
+        var domView = $entity.domView;
+        this.stageElement.removeChild(domView.element);
+        domView.element = null;
+    }],
+
+    /**
+     * Update position of DIV elements each tick
+     */
+    $update: ['$entity', function($entity) {
+        this.updatePosition($entity.domView.element, $entity.ng2D, $entity.ng2DSize);
+    }]
+});
+
+/**
+ * Get Vendor Prefix. Based on CraftyJS <http://craftyjs.com>
+ * @returns {*}
+ */
+function getVendorPrefix() {
+    var ua = navigator.userAgent.toLowerCase(),
+        match = /(webkit)[ \/]([\w.]+)/.exec(ua) ||
+            /(o)pera(?:.*version)?[ \/]([\w.]+)/.exec(ua) ||
+            /(ms)ie ([\w.]+)/.exec(ua) ||
+            /(moz)illa(?:.*? rv:([\w.]+))?/.exec(ua) || [],
+        prefix = (match[1] || match[0]);
+
+    //browser specific quirks
+    if (prefix === "moz") prefix = "Moz";
+    if (prefix === "o") prefix = "O";
+    return prefix;
+}
+
+/**
+ * Update position of DOM element in WebKit style
+ * @param element
+ * @param ng2D
+ */
+function buildUpdatePosition(prefix) {
+    var property = prefix + 'Transform';
+    return function (element, ng2D, ng2DSize) {
+        element.style[property] = 'translate3d('
+                + ~~(ng2D.x - 0.5 * ng2DSize.width) + 'px, '
+                + ~~(ng2D.y - 0.5 * ng2DSize.height) + 'px, 0)';
     }
-})();
+}
