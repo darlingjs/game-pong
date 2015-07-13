ff5a24f7bc2c7dda41033289215a5a3bef7559be
Eugene Krevenets <hyzhak@ya.ru>
2015-07-13 17:41:08 +0200

    use 2-space indent

--- gulpfile.js
+++ gulpfile.js
@@ -8,69 +8,69 @@ var sourcemaps = require('gulp-sourcemaps');
 var uglify = require('gulp-uglify');
 
 var LessPluginAutoPrefix = require('less-plugin-autoprefix'),
-    //uses Montenegro usage statistics. It accepts two-letter country code.
-    autoprefixPlugin = new LessPluginAutoPrefix({browsers: ['> 1% in ME']});
+//uses Montenegro usage statistics. It accepts two-letter country code.
+  autoprefixPlugin = new LessPluginAutoPrefix({browsers: ['> 1% in ME']});
 
 var LessPluginCleanCSS = require('less-plugin-clean-css'),
-    cleancss = new LessPluginCleanCSS({ advanced: true });
+  cleancss = new LessPluginCleanCSS({advanced: true});
 
 var paths = {
-    scripts: {
-        source: ['./js/game.js'],
-        output: {
-            path: './build/js',
-            filename: 'game.min.js'
-        }
-    },
-    styles: {
-        source: './styles/*.less',
-        output: {
-            path: './build/styles',
-            filename: 'game.css'
-        }
-    },
-    html: {
-        source: './index.html',
-        output: {
-            path: './build',
-            filename: 'index'
-        }
+  scripts: {
+    source: ['./js/game.js'],
+    output: {
+      path: './build/js',
+      filename: 'game.min.js'
     }
+  },
+  styles: {
+    source: './styles/*.less',
+    output: {
+      path: './build/styles',
+      filename: 'game.css'
+    }
+  },
+  html: {
+    source: './index.html',
+    output: {
+      path: './build',
+      filename: 'index'
+    }
+  }
 };
 
 function clean(done) {
-    del(['build'], done);
+  del(['build'], done);
 }
 
 function html() {
-    return gulp.src(paths.html.source)
-        .pipe(processhtml())
-        .pipe(debug({title: 'html:'}))
-        .pipe(gulp.dest(paths.html.output.path));
+  return gulp.src(paths.html.source)
+    .pipe(processhtml())
+    .pipe(debug({title: 'html:'}))
+    .pipe(gulp.dest(paths.html.output.path));
 }
 
 function styles() {
-    return gulp.src(paths.styles.source)
-        //TODO: sourcemap doesn't work with plugins because of bug in them
-        //.pipe(sourcemaps.init())
-        .pipe(less({
-            plugins: [cleancss, autoprefixPlugin]
-        }))
-        //.pipe(sourcemaps.write('.'))
-        .pipe(debug({title: 'styles:'}))
-        .pipe(gulp.dest(paths.styles.output.path));
+  return gulp.src(paths.styles.source)
+    //TODO: sourcemap doesn't work with plugins because of bug in them
+    //.pipe(sourcemaps.init())
+    .pipe(less({
+      plugins: [cleancss, autoprefixPlugin]
+    }))
+    //.pipe(sourcemaps.write('.'))
+    .pipe(debug({title: 'styles:'}))
+    .pipe(gulp.dest(paths.styles.output.path));
 }
 
 function scripts() {
-    return gulp.src(paths.scripts.source)
-        .pipe(sourcemaps.init())
-        .pipe(browserify({
-            //debug : !gulp.env.production
-        }))
-        .pipe(uglify())
-        .pipe(sourcemaps.write('.'))
-        .pipe(debug({title: 'scripts:'}))
-        .pipe(gulp.dest(paths.scripts.output.path));
+  return gulp.src(paths.scripts.source)
+    .pipe(sourcemaps.init())
+    .pipe(browserify({
+      //debug : !gulp.env.production
+    }))
+    .pipe(uglify())
+    .pipe(sourcemaps.write('.'))
+    .pipe(debug({title: 'scripts:'}))
+    .pipe(gulp.dest(paths.scripts.output.path));
 }
 
 gulp.task('clean', clean);
@@ -78,15 +78,15 @@ gulp.task('html', html);
 gulp.task('styles', styles);
 gulp.task('scripts', scripts);
 
-gulp.task('watch', function() {
-    gulp.watch(paths.html.source, html);
-    gulp.watch(paths.styles.source, styles);
-    gulp.watch(paths.scripts.source, scripts);
+gulp.task('watch', function () {
+  gulp.watch(paths.html.source, html);
+  gulp.watch(paths.styles.source, styles);
+  gulp.watch(paths.scripts.source, scripts);
 });
 
 gulp.task('build', gulp.series(
-    clean,
-    gulp.parallel('scripts', 'html', 'styles')
+  clean,
+  gulp.parallel('scripts', 'html', 'styles')
 ));
 
 gulp.task('default', gulp.series('build'));
